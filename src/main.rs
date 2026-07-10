6be47d76ecc8c5a797762f82a10dedf454fec65e
Mika Cohen <mjc@kernel.org>
2026-07-10 13:06:07 -0600

    Fix CLI box width for wide characters

--- src/main.rs
+++ src/main.rs
@@ -9,39 +9,13 @@ fn boxed(rendered: &str) -> String {
         .map(|line| UnicodeWidthStr::width(*line))
         .max()
         .unwrap_or(0);
-    let height = lines.len();
-    let w = width + 4;
-    let h = height + 4;
-    let mut box_data = vec![' '; w * h];
-
-    box_data[0] = '┌';
-    box_data[w - 1] = '┐';
-    box_data[(h - 1) * w] = '└';
-    box_data[(h - 1) * w + w - 1] = '┘';
-
-    for x in 1..w - 1 {
-        box_data[x] = '─';
-        box_data[(h - 1) * w + x] = '─';
+    let border = "─".repeat(width + 2);
+    let mut out = format!("┌{border}┐\n│ {} │\n", " ".repeat(width));
+    for line in lines {
+        let padding = width - UnicodeWidthStr::width(line);
+        out.push_str(&format!("│ {line}{} │\n", " ".repeat(padding)));
     }
-
-    for y in 1..h - 1 {
-        box_data[y * w] = '│';
-        box_data[y * w + w - 1] = '│';
-    }
-
-    for (y, line) in lines.iter().enumerate() {
-        for (x, c) in line.chars().enumerate() {
-            box_data[(y + 2) * w + x + 2] = c;
-        }
-    }
-
-    let mut out = String::with_capacity(box_data.len() + h);
-    for y in 0..h {
-        for x in 0..w {
-            out.push(box_data[y * w + x]);
-        }
-        out.push('\n');
-    }
+    out.push_str(&format!("│ {} │\n└{border}┘\n", " ".repeat(width)));
     out
 }
 
